98a12e38b46fcbe3348facc6c2392ae1f8ad78a3
tran nhat nam <tnn231223@gmail.com>
2024-11-13 09:35:16 +0700

    oo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Thay bằng URL của FE
+    origin: ["http://localhost:3000", "https://fe-drink.vercel.app"], // Thay bằng URL của FE
     credentials: true, // Cho phép gửi cookie cross-origin
   })
 );
